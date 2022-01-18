78a784cd2724579496fff0d9eab935c9fbd42605
SadraMoghadam <sadra_h_m@outlook.com>
2022-01-18 17:39:42 +0330

    Restaurant part mostly completed except the inheritance part

---
+++ Controller/PlaceController/Restaurant/RestaurantDetails.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace TripAdvisor.Restaurant
+{
+    public class RestaurantDetails
+    {
+        private List<string> cuisines;
+        private List<string> specialDiets;
+        private List<string> meals;
+        private List<string> features;
+
+        public List<string> Cuisines => cuisines;
+
+        public List<string> SpecialDiets => specialDiets;
+
+        public List<string> Meals => meals;
+
+        public List<string> Features => features;
+
+        public RestaurantDetails(List<string> cuisines, List<string> specialDiets, List<string> meals, List<string> features)
+        {
+            setDetails(cuisines, specialDiets, meals, features);
+        }
+        public void setDetails(List<string> cuisines, List<string> specialDiets, List<string> meals, List<string> features)
+        {
+            this.cuisines = cuisines;
+            this.specialDiets = specialDiets;
+            this.meals = meals;
+            this.features = features;
+        }
+    }
+}
